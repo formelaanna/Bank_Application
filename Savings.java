3b15ec5175223ba053548040b8ec5ac01f7197b0
Anna Formela <formela.anna7@gmail.com>
2019-11-25 22:03:27 +0100

    Add new files

---
+++ Savings.java
@@ -0,0 +1,31 @@
+package bankAccountApp;
+
+public class Savings extends Account {
+	
+	private int safetyDepositBoxID;
+	private int safetyDepositBoxKey;
+
+	public Savings(String name, String sSN, double initDeposit) {
+		super(name, sSN, initDeposit);
+		accountNumber = "1" + accountNumber;
+		setSafetyDepositBox();
+	}
+	
+	@Override
+		public void setRate() {
+		rate = getBaseRate() - .25;		
+	};
+	
+	private void setSafetyDepositBox() {
+		safetyDepositBoxID = (int) (Math.random() * Math.pow(10,3));
+	    safetyDepositBoxKey = (int)(Math.random()* Math.pow(10, 4)); 
+	}
+	
+	public void showInfo() {
+		super.showInfo();
+		System.out.println("Your savings account features :" +
+		"\nSafety Deposit Box ID : " + safetyDepositBoxID + 
+		"\nSafety Deposit Box Key : " + safetyDepositBoxKey
+				);
+	}
+}
